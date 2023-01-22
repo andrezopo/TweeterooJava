46bf53c7ad40d1306b0df5d5c7e5c04486c25994
André Zopolato <andrezopo@gmail.com>
2023-01-21 22:07:37 -0300

    :sparkles: feat: create User and Tweet repositories

---
+++ src/main/java/com/tweeteroo/tweeteroo/repository/TweetRepository.java
@@ -0,0 +1,9 @@
+package com.tweeteroo.tweeteroo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tweeteroo.tweeteroo.model.Tweet;
+
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+    
+}
