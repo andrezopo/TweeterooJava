f267cbdad7be5a5bc85969684b9d3540e2560299
André Zopolato <andrezopo@gmail.com>
2023-01-21 22:06:57 -0300

    :recycle: refactor: change User primary key from id to username

--- src/main/java/com/tweeteroo/tweeteroo/model/User.java
+++ src/main/java/com/tweeteroo/tweeteroo/model/User.java
@@ -2,14 +2,12 @@ package com.tweeteroo.tweeteroo.model;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import com.tweeteroo.tweeteroo.dto.UserDTO;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
@@ -27,18 +25,15 @@ public class User {
 
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
     @Column(length = 50, nullable = false, unique = true)
+    @Id
     private String username;
 
 
     @Column(length = 300, nullable = false)
     private String avatarUrl;
 
-    @OneToMany
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true )
     private Collection<Tweet> tweets = new ArrayList<>() {
         
     };
