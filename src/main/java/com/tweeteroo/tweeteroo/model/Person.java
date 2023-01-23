aaf1182303c2929fedf5e615fde9b9cb0447f61e
André Zopolato <andrezopo@gmail.com>
2023-01-23 20:08:10 -0300

    :bricks: build: change Person and Tweet relation for only Many-to-One on the Tweet side

--- src/main/java/com/tweeteroo/tweeteroo/model/Person.java
+++ src/main/java/com/tweeteroo/tweeteroo/model/Person.java
@@ -3,19 +3,24 @@ package com.tweeteroo.tweeteroo.model;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.hibernate.annotations.NaturalId;
+
 import com.tweeteroo.tweeteroo.dto.PersonDTO;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "persons")
 public class Person {
     
     public Person(PersonDTO data){
@@ -26,17 +31,15 @@ public class Person {
     public Person(){
 
     }
-
-    @Column(length = 50, nullable = false, unique = true)
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true, name = "username")
+    @NaturalId
     private String username;
 
 
-    @Column(length = 300, nullable = false)
+    @Column(length = 300, nullable = false, name = "avatar_url")
     private String avatarUrl;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true )
-    private Collection<Tweet> tweets = new ArrayList<Tweet>() {
-        
-    };
 }
--- src/main/java/com/tweeteroo/tweeteroo/model/Tweet.java
+++ src/main/java/com/tweeteroo/tweeteroo/model/Tweet.java
@@ -1,18 +1,25 @@
 package com.tweeteroo.tweeteroo.model;
 
 import com.tweeteroo.tweeteroo.dto.TweetDTO;
+import com.tweeteroo.tweeteroo.types.PersonTweet;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
 @Entity
 public class Tweet {
+
+    public Tweet(Person user, String text){
+        this.text = text;
+        this.user = user;
+    }
 
     public Tweet (TweetDTO data){
         this.text = data.text();
@@ -25,9 +32,10 @@ public class Tweet {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(length = 500, nullable = false)
+    @Column(length = 500, nullable = false, name = "text")
     private String text;
 
     @ManyToOne
+    @JoinColumn(name = "username")
     private Person user;
 }
