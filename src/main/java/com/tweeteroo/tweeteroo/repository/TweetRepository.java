62cf8d4a7d178b13ee83ebe03ac831790ec99a93
André Zopolato <andrezopo@gmail.com>
2023-01-23 20:12:57 -0300

    :sparkles: feat: call tweet repository default queries to findByUser and findAll Pageable

--- src/main/java/com/tweeteroo/tweeteroo/repository/TweetRepository.java
+++ src/main/java/com/tweeteroo/tweeteroo/repository/TweetRepository.java
@@ -1,9 +1,18 @@
 package com.tweeteroo.tweeteroo.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.tweeteroo.tweeteroo.model.Person;
 import com.tweeteroo.tweeteroo.model.Tweet;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
-    
+
+    Page<Tweet> findAll(Pageable pageable);
+    public List<Tweet> findByUser(Person user);
+
+
 }
