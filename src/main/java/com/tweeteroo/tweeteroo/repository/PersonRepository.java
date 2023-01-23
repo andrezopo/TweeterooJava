331b9a713c488f74399067a1bee66d3abcc20988
André Zopolato <andrezopo@gmail.com>
2023-01-23 20:09:03 -0300

    :bug: fix: change the findByUsername back to default query

--- src/main/java/com/tweeteroo/tweeteroo/repository/PersonRepository.java
+++ src/main/java/com/tweeteroo/tweeteroo/repository/PersonRepository.java
@@ -1,9 +1,12 @@
 package com.tweeteroo.tweeteroo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweeteroo.tweeteroo.model.Person;
 
 public interface PersonRepository extends JpaRepository<Person, String> {
     
+    public List<Person> findByUsername(String username);
 }
