0b517e6416383becccf84ee31512e7b7ea104477
André Zopolato <andrezopo@gmail.com>
2023-01-23 22:45:12 -0300

    :sparkles: feat: create query to get tweets by username

--- src/main/java/com/tweeteroo/tweeteroo/repository/TweetRepository.java
+++ src/main/java/com/tweeteroo/tweeteroo/repository/TweetRepository.java
@@ -2,17 +2,28 @@ package com.tweeteroo.tweeteroo.repository;
 
 import java.util.List;
 
+import org.hibernate.annotations.OrderBy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.tweeteroo.tweeteroo.model.Person;
 import com.tweeteroo.tweeteroo.model.Tweet;
+import com.tweeteroo.tweeteroo.types.CompleteTweet;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
 
+    
     Page<Tweet> findAll(Pageable pageable);
-    public List<Tweet> findByUser(Person user);
+    
+    
+    
+    List<Tweet> findByUsername(String username);
+    
+    List<Tweet> findByUsernameOrderByIdDesc(String username);
+
+    
 
 
 }
