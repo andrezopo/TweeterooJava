994ab399dadae717e908dc895f9839f6eef4dcf2
André Zopolato <andrezopo@gmail.com>
2023-01-23 22:46:03 -0300

    :sparkles: feat: create tweet service method to order the page results by id

--- src/main/java/com/tweeteroo/tweeteroo/service/TweetService.java
+++ src/main/java/com/tweeteroo/tweeteroo/service/TweetService.java
@@ -8,11 +8,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.tweeteroo.tweeteroo.dto.TweetDTO;
 import com.tweeteroo.tweeteroo.model.Person;
 import com.tweeteroo.tweeteroo.model.Tweet;
 import com.tweeteroo.tweeteroo.repository.PersonRepository;
 import com.tweeteroo.tweeteroo.repository.TweetRepository;
-import com.tweeteroo.tweeteroo.types.TweetUsername;
 
 @Service
 public class TweetService {
@@ -23,18 +23,35 @@ public class TweetService {
     @Autowired
     private PersonRepository personRepository;
 
-    public void createTweet(TweetUsername data) {
-        List<Person> user = personRepository.findByUsername(data.username);
-        if (!user.isEmpty()){
-            Tweet tweet = new Tweet();
-            tweet.setText(data.tweet);
-            tweet.setUser(user.get(0));
-            tweetRepository.save(tweet);
+    public boolean createTweet(String username , TweetDTO req) {
+        List<Person> user = personRepository.findByUsername(username);
+        try {
+            if (!user.isEmpty()){
+                Tweet tweet = new Tweet(user.get(0), req.text());
+                tweetRepository.save(tweet);
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
         }
+
+        return true;
+
         
     }
 
     public Page<Tweet> findAll(Pageable pageable) {
-        return tweetRepository.findAll(pageable);
+         return tweetRepository.findAll(pageable);
+    }
+
+    public List<Tweet> findByUsername(String username){
+        List<Tweet> userTweets = tweetRepository.findByUsernameOrderByIdDesc(username);
+
+        if (userTweets.isEmpty()){
+            return List.of();
+        }
+
+        return userTweets;
+
     }
 }
